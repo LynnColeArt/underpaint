5f0199b1356a2cac0bf7baba5de96ae7ec37189e
REDACTED <redacted@example.invalid>
2022-09-21 11:57:58 +0200

    Start upgrade to Drawpile 2.2
    Geberates the new protocol code and barely upgrades the implementation
    enough to be able to join a Drawpile 2.2 session. It's still missing
    most of the new features though, like layer groups or MyPaint brushes.
    That implementation will follow.
    
    Breaks a bunch of tests irrepably, since they rely on Drawpile 2.1
    recording formats. They'll be replaced in future commits.

--- libmsg/dpmsg/disconnect_reason.h
+++ libmsg/dpmsg/disconnect_reason.h
@@ -19,28 +19,14 @@
  * This code is based on Drawpile, using it under the GNU General Public
  * License, version 3. See 3rdparty/licenses/drawpile/COPYING for details.
  */
-#ifndef DPMSG_LAYER_DELETE_H
-#define DPMSG_LAYER_DELETE_H
-#include <dpcommon/common.h>
+#ifndef DPMSG_DISCONNECT_REASON_H
+#define DPMSG_DISCONNECT_REASON_H
 
-typedef struct DP_Message DP_Message;
-
-
-typedef struct DP_MsgLayerDelete DP_MsgLayerDelete;
-
-DP_Message *DP_msg_layer_delete_new(unsigned int context_id, int layer_id,
-                                    bool merge);
-
-DP_Message *DP_msg_layer_delete_deserialize(unsigned int context_id,
-                                            const unsigned char *buffer,
-                                            size_t length);
-
-DP_MsgLayerDelete *DP_msg_layer_delete_cast(DP_Message *msg);
-
-
-int DP_msg_layer_delete_layer_id(DP_MsgLayerDelete *mld);
-
-bool DP_msg_layer_delete_merge(DP_MsgLayerDelete *mld);
-
+typedef enum DP_DisconnectReason {
+    DP_MSG_DISCONNECT_REASON_ERROR,
+    DP_MSG_DISCONNECT_REASON_KICK,
+    DP_MSG_DISCONNECT_REASON_SHUTDOWN,
+    DP_MSG_DISCONNECT_REASON_OTHER,
+} DP_DisconnectReason;
 
 #endif
--- libmsg/dpmsg/msg_internal.h
+++ libmsg/dpmsg/msg_internal.h
@@ -19,8 +19,29 @@
  * This code is based on Drawpile, using it under the GNU General Public
  * License, version 3. See 3rdparty/licenses/drawpile/COPYING for details.
  */
-#include "user_acl.h"
-#include "user_ids.h"
+#ifndef DPMSG_MSG_INTERNAL_H
+#define DPMSG_MSG_INTERNAL_H
 #include <dpcommon/common.h>
 
-DP_MSG_USER_IDS_DEFINE(DP_MSG_USER_ACL, DP_MsgUserAcl, user_acl)
+typedef struct DP_Message DP_Message;
+
+typedef enum DP_MsgInternalType {
+    DP_MSG_INTERNAL_TYPE_RESET,
+    DP_MSG_INTERNAL_TYPE_SOFT_RESET,
+    DP_MSG_INTERNAL_TYPE_COUNT,
+} DP_MsgInternalType;
+
+
+typedef struct DP_MsgInternal DP_MsgInternal;
+
+DP_Message *DP_msg_internal_reset_new(unsigned int context_id);
+
+DP_Message *DP_msg_internal_soft_reset_new(unsigned int context_id);
+
+DP_MsgInternal *DP_msg_internal_cast(DP_Message *msg);
+
+
+DP_MsgInternalType DP_msg_internal_type(DP_MsgInternal *mi);
+
+
+#endif
