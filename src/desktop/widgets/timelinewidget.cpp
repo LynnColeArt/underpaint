drawpile/Drawpile

Normalize includes

src/desktop/widgets/timelinewidget.cpp
@@ -17,10 +17,10 @@
    along with Drawpile.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "timelinewidget.h"
-#include "canvas/timelinemodel.h"
-#include "net/envelopebuilder.h"
-#include "utils/qtguicompat.h"
+#include "desktop/widgets/timelinewidget.h"
+#include "libclient/canvas/timelinemodel.h"
+#include "libclient/net/envelopebuilder.h"
+#include "desktop/utils/qtguicompat.h"
 
 #include <QPaintEvent>
 #include <QMouseEvent>

src/libclient/canvas/selection.cpp
@@ -17,11 +17,11 @@
    along with Drawpile.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "selection.h"
-#include "net/envelopebuilder.h"
-#include "net/envelope.h"
-#include "../tools/selection.h" // for selection utilities
-#include "../rustpile/rustpile.h"
+#include "libclient/canvas/selection.h"
+#include "libclient/net/envelopebuilder.h"
+#include "libclient/net/envelope.h"
+#include "libclient/tools/selection.h" // for selection utilities
+#include "rustpile/rustpile.h"
 
 #include <QPainter>
 #include <QtMath>

LynnColeArt/underpaint

src/desktop/widgets/timelinewidget.cpp
@@ -17,9 +17,9 @@
    along with Drawpile.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "timelinewidget.h"
-#include "canvas/timelinemodel.h"
-#include "drawdance/message.h"
+#include "desktop/widgets/timelinewidget.h"
+#include "libclient/canvas/timelinemodel.h"
+#include "libclient/drawdance/message.h"
 
 #include <QPaintEvent>
 #include <QMouseEvent>

src/libclient/canvas/selection.cpp
@@ -21,9 +21,9 @@ extern "C" {
 #include <dpmsg/message.h>
 }
 
-#include "selection.h"
-#include "../tools/selection.h" // for selection utilities
-#include "drawdance/message.h"
+#include "libclient/canvas/selection.h"
+#include "libclient/tools/selection.h" // for selection utilities
+#include "libclient/drawdance/message.h"
 
 #include <QPainter>
 #include <QtMath>